6daea2f485238108da3994166debe81ae9d3c40b
manishimweclaudine <claudinemanishimwe2002@gmail.com>
2025-06-09 14:23:23 -0800

    second commit

---
+++ 22RP02928/operator.dart
@@ -0,0 +1,10 @@
+void main() {
+  int a = 10;
+  int b = 3;
+
+  print("Add: ${a + b}");
+  print("Subtract: ${a - b}");
+  print("Multiply: ${a * b}");
+  print("Divide: ${a / b}");
+  print("Modulus: ${a % b}");
+}
--- 22RP02928/variables.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  int a = 10;
-  int b = 5;
-  int sum = a + b;
-
-  print("The sum is $sum");
-}
