c0c71b47b01f713736649ba953553ee264aab048
manishimweclaudine <claudinemanishimwe2002@gmail.com>
2025-06-09 16:41:30 -0800

    Dart Language Features

---
+++ 22RP02928/late variable.dart
@@ -0,0 +1,8 @@
+void main() {
+  late String username;
+
+  // some later logic
+  username = 'Alice';
+
+  print('Logged in as: $username');
+}
---
+++ 22RP02928/null.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = 'Alice'; // Non-nullable
+  print(name);
+
+  String? nickname; // Nullable (can be null)
+  print(nickname); // Output: null
+}
