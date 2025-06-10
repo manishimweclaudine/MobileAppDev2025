213e3fb4b964542161ce7f280c1ccbfc12491f4f
manishimweclaudine <claudinemanishimwe2002@gmail.com>
2025-06-09 17:25:40 -0800

    Loops & Flow Control

---
+++ 22RP02928/Break Statement.dart
@@ -0,0 +1,8 @@
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    if (i == 4) {
+      break; // Exit loop when i is 4
+    }
+    print("i = $i");
+  }
+}
---
+++ 22RP02928/for-In loop.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<String> fruits = ['Apple', 'Banana', 'Mango'];
+
+  for (var fruit in fruits) {
+    print("Fruit: $fruit");
+  }
+}
